e2a27cf3049c8189adec3ff5863373a6afa7091d
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-07-19 20:44:42 -0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">Home</a>
+      </header>
+      <main>
+        lista de producto     
+        </main>
+
+    </div>
+  );
+}
+
+export default App;
