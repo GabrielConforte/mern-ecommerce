e3b5b2be10741f27ec8dad4bcb5144f9622d044a
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-07-19 23:27:20 -0300

    Se añade Backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import  express  from "express";
+import data from "./data.js";
+import  logger  from "./config/logger.js";
+
+const app = express();
+
+app.get("/api/product", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => logger.info(`Server iniciado desde  http://localhost:${port}`));
