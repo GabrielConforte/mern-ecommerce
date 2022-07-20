c634a7023e3d292a939435b092973804856aea54
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-07-19 22:18:44 -0300

    sumary, esto se va a borrar igual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+import './index.css';
 function App() {
   return (
     <div>
@@ -5,7 +7,25 @@ function App() {
         <a href="/">Home</a>
       </header>
       <main>
-        lista de producto     
+        <h1>Destacados</h1>
+        <div className="productoContenedor">
+        {
+          data.products.map(product => (
+            <div className="productoItem" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+              </a>
+              <div className="productInfo">
+              <a href={`/product/${product.slug}`}>
+               <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>{product.price}</p></a>
+                <button>Añadir a Carrito</button>
+              </div>
+            </div>
+          ))
+
+        }</div>
         </main>
 
     </div>
